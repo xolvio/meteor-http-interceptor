b0c669f8b25b097593295929df8cae000bdb4bb7
Workflow <florian.peter@gmx.at>
2015-08-04 12:27:00 +0200

    Client side intercepting: refactoring. #6

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@
  * Intercepting client side calls to hosts registered with HttpInterceptor.registerInterceptor()
  */
 var http = Npm.require('http');
-var url = Npm.require('url');
+var URL = Npm.require('url');
 
 HttpInterceptor.registerInterceptor('http://res.cloudinary.com', Meteor.absoluteUrl('fake.res.cloudinary.com'));
 Meteor._debug(HttpInterceptor.getInterceptors());
@@ -11,28 +11,26 @@ Meteor._debug(HttpInterceptor.getInterceptors());
 http.createServer(function(request, response) {
     var interceptors = HttpInterceptor.getInterceptors();
     var oldUrl = request.url;
-    var newUrl = oldUrl;
-    var oldHost = request.headers['host'];
-    var host = oldHost;
+    var url = oldUrl;
+    var host = request.headers['host'];
+    var proxy, proxy_request;
 
-    _.each(interceptors, function (newHost, originalHost) {
-        if (newUrl.indexOf(originalHost) > -1) {
-            Meteor._debug(newUrl, originalHost);
-            newUrl = newUrl.replace(originalHost, newHost);
-            Meteor._debug('REROUTING', oldUrl, '->', newUrl);
-            host = url.parse(newHost).hostname;
+    _.each(interceptors, function(newHost, originalHost) {
+        if (url.indexOf(originalHost) > -1) {
+            Meteor._debug(url, originalHost);
+            url = url.replace(originalHost, newHost);
+            Meteor._debug('CAPTURING', oldUrl, '->', url);
+            host = URL.parse(newHost).hostname;
             Meteor._debug(host);
         }
     });
 
-
-
-    var proxy = http.createClient(80, host);
+    proxy = http.createClient(80, host);
     Meteor._debug('Just created a client for: ' + host);
-    var proxy_request = proxy.request(request.method, newUrl, request.headers);
+    proxy_request = proxy.request(request.method, url, request.headers);
     Meteor._debug('Using method: ' + request.method);
-    Meteor._debug('Accessing URL: ' + newUrl);
-    proxy_request.addListener('response', function (proxy_response) {
+    Meteor._debug('Accessing URL: ' + url);
+    proxy_request.addListener('response', function(proxy_response) {
         proxy_response.addListener('data', function(chunk) {
             response.write(chunk, 'binary');
         });
